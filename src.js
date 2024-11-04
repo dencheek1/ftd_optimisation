7d0b8b3ac741c5d15dc66a47931c4dd950bf95f4
Dzianis <dzianis@localhost.localdomain>
2024-11-04 21:06:13 +0300

    first commit

---
+++ src.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function generateField(size = 0){
+    const field = document.getElementById('field');
+    let counter = 0;
+    for(let i = size*size; i > 0; i --){
+
+        let cell = document.createElement(div);
+        cell.setAttribute('cell', ``)
+
+    }
+    console.log(counter);
+    return null;
+}
+
+console.log("Connected");
+generateField(2);
